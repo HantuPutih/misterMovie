9c936f0c71015b9e85718f5566c336354bacd974
Farid Juliano <thanatos.juliano@gmail.com>
2022-03-10 08:28:57 +0700

    footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,13 +1,13 @@
 <template>
   <footer class="footer">
-    <div>
+    <div class="c1">
       ©  2022 MoovieTime. All rights reserved.
     </div>
-    <div>
+    <div class="c2">
       <img src=".././assets/moovietime-logo-grey.svg" alt="movie icon">
     </div>
-    <div>
-      Made with VUE 3
+    <div class="c3">
+      Made with vue 3
     </div>
   </footer>
 </template>
@@ -27,12 +27,33 @@ export default {
   background-color: #15191e;
   height: 160px;
   width: 1440px;
-  display: flex;
+  display: grid;
+  column-gap: 50px;
+  //display: flex;
   align-items: center;
-  justify-content: space-evenly;
+  //justify-content: space-evenly;
   font-style: normal;
   font-weight: normal;
   font-size: 1rem;
   color: #929292;
+  .c1 {
+    grid-column: 1/2;
+    text-align: center;
+    //grid-template-columns: repeat(4, 1fr);
+  }
+  .c2 {
+    grid-column:  2/3;
+    text-align: center;
+    //grid-template-columns: repeat(4, 1fr);
+
+
+  }
+  .c3 {
+    grid-column:  3/4;
+    text-align: center;
+    //grid-template-columns: repeat(4, 1fr);
+
+
+  }
 }
 </style>
